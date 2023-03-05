643e5e70ba3cd923c37ea7b9cde46d7882a81d7e
msr <mar_r@mail.ru>
2023-03-05 13:00:32 +0300

    create object

---
+++ js/script.js
@@ -0,0 +1,32 @@
+app = {
+  difficulty: '',
+  progress: '',
+  duration: '',
+  allCards: {},
+  selectedCards: {}
+}
+
+const difficultyScreen = document.querySelector('.difficulty-screen');
+const gameScreen = document.querySelector('.game-screen');
+
+const difficulty = difficultyScreen.querySelector('.difficulty');
+const difficultyButtons = difficulty.querySelectorAll('.difficulty__btn');
+const btnStart = difficultyScreen.querySelector('.btn-start');
+
+difficulty.addEventListener('click', (event) => {
+  target = event.target;
+  if (!target.classList.contains('difficulty__btn')) {
+    return;
+  }
+  difficultyButtons.forEach(btn => {
+    btn.classList.remove('difficulty__btn_selected');
+  });
+  target.classList.add('difficulty__btn_selected');
+
+  app.difficulty = +target.dataset.value;
+});
+
+btnStart.addEventListener('click', () => {
+  difficultyScreen.classList.add('hidden');
+  gameScreen.classList.remove('hidden');
+});
