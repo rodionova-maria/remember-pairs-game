79f35cb85121820f1044374656eb76f84aaba408
msr <mar_r@mail.ru>
2023-03-11 16:07:01 +0300

    with linters

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const ALL_CARDS = [
+let ALL_CARDS = [
   'hearts-6.svg',
   'hearts-7.svg',
   'hearts-8.svg',
@@ -38,11 +38,111 @@ const ALL_CARDS = [
 ];
 
 const app = {
-  difficulty: 3,
+  difficulty: 4,
   duration: '',
   generatedCards: [],
-  selectedCards: {}
-}
+  selectedCards: {},
+};
 
 const difficultyScreen = document.querySelector('.difficulty-screen');
+
+const difficulty = difficultyScreen.querySelector('.difficulty');
+const difficultyButtons = difficulty.querySelectorAll('.difficulty__btn');
+const btnStart = difficultyScreen.querySelector('.btn-start');
+
+difficulty.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.classList.contains('difficulty__btn')) {
+    return;
+  }
+  difficultyButtons.forEach((btn) => {
+    btn.classList.remove('difficulty__btn_selected');
+  });
+  target.classList.add('difficulty__btn_selected');
+  app.difficulty = +target.dataset.value;
+});
+
 const gameScreen = document.querySelector('.game-screen');
+
+const getRandom = (max) => Math.floor(Math.random() * max);
+
+const generateCards = (difLevel) => {
+  do {
+    let randomCardId = getRandom(ALL_CARDS.length);
+    if (!app.generatedCards.includes(randomCardId)) {
+      app.generatedCards.push(randomCardId);
+    }
+  } while (app.generatedCards.length < difLevel);
+  app.generatedCards = [...app.generatedCards, ...app.generatedCards];
+};
+
+const hideCards = () => {
+  const cards = gameScreen.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.src = '../images/shirt.svg';
+  });
+};
+
+const mix = (array) => {
+  return (array = array.sort(() => Math.random() - 0.5));
+};
+
+const renderCards = () => {
+  const cards = gameScreen.querySelector('.game__cards');
+  cards.innerHTML = '';
+  app.generatedCards.forEach((id) => {
+    const card = document.createElement('img');
+    card.classList.add('card');
+    card.setAttribute('data-id', id);
+    card.src = `../images/${ALL_CARDS[id]}`;
+    cards.appendChild(card);
+  });
+};
+
+btnStart.addEventListener('click', () => {
+  difficultyScreen.classList.add('hidden');
+  gameScreen.classList.remove('hidden');
+
+  const gameTimer = gameScreen.querySelector('.game__panel-timer');
+
+  let secs,
+    now,
+    timer,
+    mins = 0;
+
+  function startTimer() {
+    now = Date.now();
+    mins = 0;
+    timer = setInterval(function () {
+      secs = Math.floor((Date.now() - now) / 1000);
+      if (secs === 60) {
+        now = Date.now();
+        mins++;
+      }
+      if (secs < 10) {
+        secs = '0' + secs;
+      }
+      gameTimer.innerHTML = mins + '.' + secs;
+    });
+  }
+
+  const showHide5sec = () => {
+    clearInterval(timer);
+    app.duration = `${mins}.${secs}`;
+    gameTimer.innerHTML = '0.00';
+    renderCards();
+    setTimeout(function () {
+      hideCards();
+      startTimer();
+    }, 3000);
+  };
+
+  generateCards(app.difficulty);
+  app.generatedCards = mix(app.generatedCards);
+  showHide5sec();
+
+  const btnReset = gameScreen.querySelector('.btn-reset');
+  btnReset.addEventListener('click', () => {
+    showHide5sec();
+  });
+});
