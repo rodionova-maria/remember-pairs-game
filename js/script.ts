29e3028b468c9b22d6d282095afd53c237f48f54
Мария Радионова <rodionova@forsite.ru>
2023-03-17 17:59:10 +0300

    fix

--- js/script.ts
+++ js/script.ts
@@ -1,10 +1,12 @@
 import { startTimer } from './timer';
 import { stopTimer } from './timer';
+import { showResult } from './result';
 
 import '../css/vars.scss';
 import '../css/main.scss';
 import '../css/difficulty.scss';
 import '../css/play.scss';
+import '../css/result.scss';
 
 let ALL_CARDS = [
   'hearts-6.svg',
@@ -45,56 +47,63 @@ let ALL_CARDS = [
   'clubs-ace.svg',
 ];
 
-const app = {
+type App = {
+  difficulty: number;
+  duration: string | null;
+  generatedCards: number[];
+  selectedCards: number[];
+  status: string;
+};
+
+export const app: App = {
   difficulty: 4,
   duration: '',
   generatedCards: [],
   selectedCards: [],
-  timeoutId: undefined,
+  status: '',
 };
 
-const difficultyScreen = document.querySelector('.difficulty-screen');
-const difficulty: Element | null =
-  difficultyScreen.querySelector('.difficulty');
-const difficultyButtons: Element | null =
-  difficulty.querySelectorAll('.difficulty__btn');
-const btnStart: Element | null = difficultyScreen.querySelector('.btn-start');
+export const gameScreen = document.querySelector('.game-screen');
+export const difficultyScreen = document.querySelector('.difficulty-screen');
+const difficulty = difficultyScreen?.querySelector('.difficulty');
+const difficultyButtons = difficulty?.querySelectorAll('.difficulty__btn');
+const btnStart = difficultyScreen?.querySelector('.btn-start');
+const gameTimer = gameScreen?.querySelector('.game__panel-timer') as HTMLDivElement;
 
-const gameScreen = document.querySelector('.game-screen');
-
-difficulty.addEventListener('click', (event) => {
-  const target = event.target;
+difficulty?.addEventListener('click', (event) => {
+  const target = event.target as HTMLButtonElement;
   if (!target.classList.contains('difficulty__btn')) {
     return;
   }
-  difficultyButtons.forEach((btn) => {
+  difficultyButtons?.forEach((btn) => {
     btn.classList.remove('difficulty__btn_selected');
   });
   target.classList.add('difficulty__btn_selected');
-  app.difficulty = +target.dataset.value;
+  app.difficulty = Number(target.dataset.value);
 });
 
-btnStart.addEventListener('click', () => {
-  difficultyScreen.classList.add('hidden');
-  gameScreen.classList.remove('hidden');
+btnStart?.addEventListener('click', () => {
+  difficultyScreen?.classList.add('hidden');
+  gameScreen?.classList.remove('hidden');
 
   generateCards(app.difficulty);
   app.generatedCards = mix(app.generatedCards);
   showHide5sec();
 
-  const btnReset = gameScreen.querySelector('.btn-reset');
-  btnReset.addEventListener('click', () => {
+  const btnReset = gameScreen?.querySelector('.btn-reset');
+  btnReset?.addEventListener('click', () => {
     showHide5sec();
   });
 });
 
 const getRandom = (max: number) => Math.floor(Math.random() * max);
 
-const mix = (array: string[]) => {
+const mix = (array: number[]) => {
   return (array = array.sort(() => Math.random() - 0.5));
 };
 
 const generateCards = (difLevel: number) => {
+  app.generatedCards = [];
   do {
     let randomCardId = getRandom(ALL_CARDS.length);
     if (!app.generatedCards.includes(randomCardId)) {
@@ -105,54 +114,66 @@ const generateCards = (difLevel: number) => {
 };
 
 function hideCards() {
-  const cards = gameScreen.querySelectorAll('.card');
-  cards.forEach((card) => {
-    card.src = './static/shirt.svg';
+  const cards = gameScreen?.querySelectorAll('.card');
+  cards?.forEach((card) => {
+    const el = card as HTMLImageElement;
+    el.src = './static/shirt.svg';
   });
+}
+
+function showHide5sec() {
+  app.status = '';
+  gameTimer.textContent = '0.0';
+  stopTimer();
+  renderCards();
+  setTimeout(function () {
+    hideCards();
+    startTimer(gameTimer);
+  }, 3000);
 }
 
 const renderCards = () => {
   app.selectedCards = [];
   let clicksCounter = 0;
-  const cards = gameScreen.querySelector('.game__cards');
+  const cards = gameScreen?.querySelector('.game__cards') as HTMLElement;
   cards.innerHTML = '';
   app.generatedCards.forEach((id) => {
-    const card = document.createElement('img');
+    const card = new Image();
     card.classList.add('card');
-    card.setAttribute('data-id', id);
+    card.setAttribute('data-id', String(id));
     card.src = `./static/${ALL_CARDS[id]}`;
     cards.appendChild(card);
 
-    function cardClickHandler() {
-      this.src = `./static/${ALL_CARDS[id]}`;
+    function cardClickHandler(event: Event) {
+      if (app.status === 'loose' || app.status === 'win') {
+        return;
+      }
+      const target = event.target as HTMLImageElement;
+
+      target.src = `./static/${ALL_CARDS[id]}`;
       if (clicksCounter % 2 === 0) {
         app.selectedCards.push(id);
         clicksCounter++;
       } else {
         if (app.selectedCards[app.selectedCards.length - 1] !== id) {
-          stopTimer(app.timeoutId);
-          alert('Игра окончена! Вы проиграли.');
+          stopTimer();
+          app.status = 'loose';
+          app.duration = gameTimer.textContent;
+          showResult();
         } else {
           app.selectedCards.push(id);
           clicksCounter++;
         }
       }
       if (app.selectedCards.length === app.generatedCards.length) {
-        stopTimer(app.timeoutId);
-        alert('Выигрыш.');
+        stopTimer();
+        app.status = 'win';
+        app.duration = gameTimer.textContent;
+        showResult();
       }
-      this.removeEventListener('click', cardClickHandler);
+      target.removeEventListener('click', cardClickHandler);
     }
 
     card.addEventListener('click', cardClickHandler);
   });
 };
-
-function showHide5sec() {
-  stopTimer();
-  renderCards();
-  setTimeout(function () {
-    hideCards();
-    startTimer();
-  }, 3000);
-}
--- js/timer.ts
+++ js/timer.ts
@@ -1,21 +1,21 @@
-export function startTimer(timerId?: number) {
-  if (timerId) {
+let timeoutId = 0;
+
+export function startTimer(node: Element) {
+  if (timeoutId) {
     return;
   }
-  const gameTimer = gameScreen.querySelector('.game__panel-timer');
-  gameTimer.textContent = '0.00';
   const startTime = new Date().getTime();
   const run = () => {
     const time = new Date().getTime() - startTime;
-    gameTimer.textContent = (time / 1000).toFixed(1);
-    timerId = window.setTimeout(run, 50);
+    node.textContent = (time / 1000).toFixed(1);
+    timeoutId = window.setTimeout(run, 50);
   };
   run();
 }
 
-export function stopTimer(timerId?: number) {
-  if (timerId) {
-    clearTimeout(timerId);
-    timerId = undefined;
+export function stopTimer() {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    timeoutId = 0;
   }
 }
